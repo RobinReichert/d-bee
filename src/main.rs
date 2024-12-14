ec05bcdffe479dd05a0a785f4b2a47518b391f18
Robin Reichert <robin@reichert-online.eu>
2024-12-14 12:21:40 +0100

    added create table, impoved other code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod storage;
 
 fn main() {
     let fm = storage::FileManager::new();
-    let _ = fm.create_database("helloworld");
+    let _ = fm.create_database("hello");
+    let _ = fm.create_table("hello", "world");
 }
 
--- src/storage.rs
+++ src/storage.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::{self, Error}, path::PathBuf};
+use std::{fs::{self, File}, io::{self, Error}, path::PathBuf};
 use dirs::home_dir;
 
 pub struct FileManager {
@@ -7,22 +7,39 @@ pub struct FileManager {
 
 impl FileManager {
     pub fn new() -> FileManager {
-        let mut path = home_dir().expect("Failed to find home directory");
+        let mut path = home_dir().expect("Unable to determine the home directory; ensure $HOME is set");
         path.push(".d-bee");
         FileManager { base_path : path}
     }
 
     pub fn create_database(&self, name :&str) -> io::Result<()> {
-        let mut path = self.base_path.clone(); 
-        path.push("databases");
-        path.push(name);
+        let path = self.base_path.join("databases").join(name);
         if path.exists() {
-            return Err(Error::other("database already exists"));
+            return Err(Error::new(io::ErrorKind::AlreadyExists, "database with this name already exists"));
         }
         fs::create_dir_all(path)?;  
         return Ok(());
     }
 
+    pub fn create_table(&self, name : &str, database : &str) -> io::Result<()> {
+        if !self.database_exists(database) {
+            return Err(Error::new(io::ErrorKind::NotFound, "database does not exist"));
+        }
+        let path = self.base_path.join("databases").join(database).join(name);
+        File::create(path)?;
+        return Ok(());
+    }
+
+    fn database_exists(&self, database : &str) -> bool {
+        let path = self.base_path.join("databases").join(database);
+        return path.exists();
+    }
+
+    #[warn(dead_code)]
+    fn table_exists(&self, name :&str, database : &str) -> bool {
+        let path = self.base_path.join("databases").join(database).join(name);
+        return path.exists();
+    }
 
 }
 
@@ -32,7 +49,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_database_panic_test() {
+    fn create_database_normal_test() {
         let fm = FileManager::new();
         fm.create_database("test").expect("Failed to create database dir");
     }
@@ -41,7 +58,13 @@ mod tests {
     fn create_database_dublicate_test() {
         let fm = FileManager::new();
         fm.create_database("double").expect("Failed to create database dir");
-        fm.create_database("double").expect_err("database already exists");
+        fm.create_database("double").expect_err("database with this name already exists");
     }
-
+    
+    #[test]
+    fn create_table_should_work_test() {
+        let fm = FileManager::new();
+        fm.create_database("table_test").expect("Failed to create database dir");
+        fm.create_table("test", "table_test").expect("Failed to create database");
+    }
 }
