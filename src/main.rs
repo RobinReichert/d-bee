2fed5e2276a210a3e6e57f45b5811f35e59bce29
Robin Reichert <robin@reichert-online.eu>
2024-12-14 16:51:57 +0100

    changed everything

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod storage;
 
 fn main() {
-    let fm = storage::FileManager::new();
-    let _ = fm.create_database("hello");
-    let _ = fm.create_table("hello", "world");
 }
 
--- src/storage.rs
+++ src/storage.rs
@@ -1,44 +1,39 @@
-use std::{fs::{self, File}, io::{self, Error}, path::PathBuf};
+#![allow(unused)]
+
+use std::{fs::{File, OpenOptions},  io::{self, Read, Seek, SeekFrom, Write}, path::PathBuf};
 use dirs::home_dir;
 
 pub struct FileManager {
-     base_path : PathBuf
+    base_path : PathBuf,
 }
 
 impl FileManager {
+
     pub fn new() -> FileManager {
-        let mut path = home_dir().expect("Unable to determine the home directory; ensure $HOME is set");
-        path.push(".d-bee");
-        FileManager { base_path : path}
+       let path = home_dir().unwrap().join(".d-bee");
+        return FileManager {base_path: path};
     }
 
-    pub fn create_database(&self, name :&str) -> io::Result<()> {
-        let path = self.base_path.join("databases").join(name);
-        if path.exists() {
-            return Err(Error::new(io::ErrorKind::AlreadyExists, "database with this name already exists"));
-        }
-        fs::create_dir_all(path)?;  
-        return Ok(());
+    pub fn create_file(&self, path : &PathBuf) -> io::Result<File> {
+        File::create_new(path)
     }
 
-    pub fn create_table(&self, name : &str, database : &str) -> io::Result<()> {
-        if !self.database_exists(database) {
-            return Err(Error::new(io::ErrorKind::NotFound, "database does not exist"));
-        }
-        let path = self.base_path.join("databases").join(database).join(name);
-        File::create(path)?;
-        return Ok(());
+    pub fn delete_file(&self, path : &PathBuf) -> io::Result<()> {
+        std::fs::remove_file(path)
     }
 
-    fn database_exists(&self, database : &str) -> bool {
-        let path = self.base_path.join("databases").join(database);
-        return path.exists();
+    pub fn read_at(&self, path : &PathBuf, at : u64, length : usize) -> io::Result<Vec<u8>> {
+        let mut file = OpenOptions::new().read(true).open(path)?;
+        file.seek(SeekFrom::Start(at))?;
+        let mut buffer = vec![0; length];
+        file.read_exact(&mut buffer)?;
+        return Ok(buffer);
     }
 
-    #[warn(dead_code)]
-    fn table_exists(&self, name :&str, database : &str) -> bool {
-        let path = self.base_path.join("databases").join(database).join(name);
-        return path.exists();
+    pub fn write_at(&self, path : &PathBuf, at : u64, data : Vec<u8>) -> io::Result<()> {
+        let mut file = OpenOptions::new().write(true).open(path)?;
+        file.seek(SeekFrom::Start(at))?;
+        return file.write_all(&data);
     }
 
 }
@@ -49,22 +44,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_database_normal_test() {
+    fn create_and_delete_test() {
         let fm = FileManager::new();
-        fm.create_database("test").expect("Failed to create database dir");
+        fm.create_file(&fm.base_path.join("file.rs"));
+        fm.delete_file(&fm.base_path.join("file.rs"));
     }
 
     #[test]
-    fn create_database_dublicate_test() {
+    fn write_and_read_test() {
         let fm = FileManager::new();
-        fm.create_database("double").expect("Failed to create database dir");
-        fm.create_database("double").expect_err("database with this name already exists");
-    }
-    
-    #[test]
-    fn create_table_should_work_test() {
-        let fm = FileManager::new();
-        fm.create_database("table_test").expect("Failed to create database dir");
-        fm.create_table("test", "table_test").expect("Failed to create database");
+        fm.create_file(&fm.base_path.join("test.txt")).unwrap();
+        let data :Vec<u8> = "hello world".into();
+        fm.write_at(&fm.base_path.join("test.txt"), 0, data.clone()).unwrap();
+        assert_eq!(fm.read_at(&fm.base_path.join("test.txt"), 0, data.len()).unwrap(), data);
     }
 }
