6bdb131a2abb16638644b714eab0492db6ff188f
Robin Reichert <robin@reichert-online.eu>
2025-02-07 01:17:02 +0100

    added tests for query

--- src/query.rs
+++ src/query.rs
@@ -31,7 +31,7 @@ pub mod parser {
             Sequence(Vec<Symbol>),
         }
 
-        #[derive(Debug)]
+        #[derive(Debug, Clone)]
         enum Argument {
             Single(String),
             List(Vec<String>),
@@ -70,7 +70,9 @@ pub mod parser {
         }
 
 
+        #[derive(Debug, Clone)]
         pub struct Query {
+            plan: HashMap<String, Argument>
         }
 
 
@@ -80,23 +82,19 @@ pub mod parser {
         impl Query {
 
 
-            pub fn from(s: String) {
+            pub fn from(s: String) -> std::io::Result<Query> {
                 let col_names : Symbol = Sequence(vec![t("("), r(v("column")), t(")")]);
                 let col_values : Symbol = Sequence(vec![t("("), r(v("value")), t(")")]);
                 let insert_values : Symbol = Option(vec![Sequence(vec![col_names.clone(), t("values"), col_values.clone()]), Sequence(vec![t("values"), col_values.clone()])]);
                 let insert : Symbol = Sequence(vec![nt("insert", "command"), t("into"), v("table"), insert_values]);
                 let operator : Symbol = Option(vec![nt("==", "operator"), nt("<", "operator"), nt("<=", "operator"), nt(">", "operator"), nt(">=", "operator")]);
                 let predicate : Symbol = Sequence(vec![v("predicate_column_name"), operator.clone(), v("predicate_value")]);
-                let columns : Symbol = Option(vec![t("*"), col_names.clone()]);
-                let select : Symbol = Sequence(vec![t("select"), columns, t("from"), v("table"), Option(vec![Sequence(vec![t("where"), predicate.clone()]) ,Sequence(vec![]) ])]);
+                let columns : Symbol = Option(vec![t("*"), Sequence(vec![v("column"), r(v("column"))])]);
+                let select : Symbol = Sequence(vec![nt("select", "command"), columns, t("from"), v("table"), Option(vec![Sequence(vec![t("where"), predicate.clone()]) ,Sequence(vec![]) ])]);
                 let query : Symbol = Sequence(vec![Option(vec![insert, select]), t(";")]);
-                let regex = Regex::new(r"\S+|([();])").unwrap();
+                let regex = Regex::new(r"\w+|[();*]|(>=)|>|(==)|<|(<=)").unwrap();
                 let mut input : Vec<String> = regex.find_iter(&s).map(|x| {x.as_str()}).map(|x| {x.to_string()}).collect();
-                println!("input: {:?}", input);
-                let res = Self::solve(vec![query], input).unwrap();
-                for stack_value in res {
-                    println!("{:?}", stack_value);
-                }
+                return Ok(Query {plan: Self::solve(vec![query], input)?});
             }
 
 
@@ -120,7 +118,6 @@ pub mod parser {
                         if val == String::from(input.pop().ok_or_else(|| {
                             Error::new(ErrorKind::InvalidInput, "input was too short")
                         })?) {
-                            println!("{}", val);
                             let mut res = Self::solve(stack, input)?;
                             if let Some(existing) = res.insert(id.clone(), Argument::Single(val.clone())) {
                                 res.remove(&val); 
@@ -185,13 +182,89 @@ pub mod parser {
 
 
         #[test]
-        fn first_test(){
-            Query::from("insert into test values ( 1 2 2 ) ;".to_string());
+        fn test_valid_insert() {
+            let result = Query::from("insert into test values (1 2 3);".to_string());
+            assert!(result.is_ok(), "Valid insert query should not return an error");
         }
 
+        #[test]
+        fn test_valid_select() {
+            let result = Query::from("select * from users where age > 30;".to_string());
+            assert!(result.is_ok(), "Valid select query should not return an error");
+        }
 
+        #[test]
+        fn test_missing_semicolon() {
+            let result = Query::from("insert into test values (1 2 3)".to_string());
+            assert!(result.is_err(), "Query without semicolon should return an error");
+        }
+
+        #[test]
+        fn test_unknown_keyword() {
+            let result = Query::from("update test set a = 1;".to_string());
+            assert!(result.is_err(), "Unsupported 'update' query should return an error");
+        }
+
+        #[test]
+        fn test_valid_insert_with_columns() {
+            let result = Query::from("insert into test (col1 col2) values (1 2);".to_string());
+            assert!(result.is_ok(), "Valid insert query with column names should not return an error");
+        }
+
+        #[test]
+        fn test_valid_select_with_columns() {
+            let result = Query::from("select col1, col2 from users where age >= 25;".to_string());
+            assert!(result.is_ok(), "Valid select query with column names should not return an error");
+        }
+
+        #[test]
+        fn test_valid_insert_multiple_values() {
+            let result = Query::from("insert into test values (1 2 3) values (4 5 6);".to_string());
+            assert!(result.is_ok(), "Valid insert query with multiple values should not return an error");
+        }
+
+        #[test]
+        fn test_valid_select_without_where() {
+            let result = Query::from("select col1, col2 from users;".to_string());
+            assert!(result.is_ok(), "Valid select query without where clause should not return an error");
+        }
+
+        #[test]
+        fn test_valid_insert_with_extra_spaces() {
+            let result = Query::from("  insert   into    test   values   (  1   2   3   )  ; ".to_string());
+            assert!(result.is_ok(), "Valid insert query with extra spaces should not return an error");
+        }
+
+        #[test]
+        fn test_invalid_insert_missing_values() {
+            let result = Query::from("insert into test (col1 col2) (1 2);".to_string());
+            assert!(result.is_err(), "Insert query missing 'values' keyword should return an error");
+        }
+
+        #[test]
+        fn test_invalid_insert_wrong_order() {
+            let result = Query::from("insert values (1 2 3) into test;".to_string());
+            assert!(result.is_err(), "Insert query with incorrect syntax should return an error");
+        }
+
+        #[test]
+        fn test_invalid_select_missing_from() {
+            let result = Query::from("select col1, col2 users where age > 25;".to_string());
+            assert!(result.is_err(), "Select query missing 'from' keyword should return an error");
+        }
+
+        #[test]
+        fn test_invalid_select_no_columns() {
+            let result = Query::from("select from users;".to_string());
+            assert!(result.is_err(), "Select query without column list or '*' should return an error");
+        }
+
+        #[test]
+        fn test_invalid_where_condition_incomplete() {
+            let result = Query::from("select * from users where age > ;".to_string());
+            assert!(result.is_err(), "Select query with incomplete WHERE clause should return an error");
+        }
     }
-
 
 
 }
